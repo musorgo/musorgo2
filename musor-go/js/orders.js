6fd1b3ab13ed3651ff30a1292711c6cc352ceb35
musorgo <designer-geek@yandex.ru>
2026-04-21 18:16:31 +0500

    Delete musor-go directory

--- musor-go/js/orders.js
+++
@@ -1,9 +0,0 @@
-import { db } from "./firebase.js";
-
-export async function createOrder(data) {
-  return await db.collection("orders").add({
-    ...data,
-    status: "new",
-    createdAt: new Date().toISOString()
-  });
-}
